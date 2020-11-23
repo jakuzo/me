85bf9763ca6c77a9d7ffbcb0020d4cee26bf7b8d
Jack Lewis <jackcampanella08@gmail.com>
2020-11-22 19:02:42 -0700

    First push

---
+++ src/Views/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    color: #e8e6e6;
+`;
+
+export default function Default() {
+	return (
+		<Wrapper>
+			<h1>Some text </h1>
+			<h1>Some text </h1>
+			<h1>Some text </h1>
+			<h1>Some text </h1>
+		</Wrapper>
+	);
+}
